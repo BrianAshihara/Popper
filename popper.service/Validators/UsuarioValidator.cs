eca6ac1cd66ed3ddfcde2e4a809bf6498d32bc53
brian <brian.c.a@gmail.com>
2023-12-07 09:58:44 -0300

    a

--- popper.service/Validators/UsuarioValidator.cs
+++ popper.service/Validators/UsuarioValidator.cs
@@ -10,9 +10,6 @@ namespace popper.Service.Validators
             RuleFor(c => c.Nome)
                 .NotEmpty().WithMessage("Por favor informe o nome.")
                 .NotNull().WithMessage("Por favor informe o nome.");
-            RuleFor(c => c.Endereco)
-                .NotEmpty().WithMessage("Por favor informe o endereco.")
-                .NotNull().WithMessage("Por favor informe o endereco.");
         }
     }
 }
