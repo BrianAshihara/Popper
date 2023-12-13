27421248764b4e58b783b2fe3071637f6f4e0306
Brian <brian09.c.a@gmail.com>
2023-12-13 08:26:45 -0300

    quase pronto...

--- popper.app/Cadastros/CadastroChamado.cs
+++ popper.app/Cadastros/CadastroChamado.cs
@@ -2,16 +2,7 @@
 using popper.domain.Base;
 using popper.domain.Entities;
 using popper.Service.Validators;
-using ReaLTaiizor.Controls;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+
 
 namespace popper.app.Cadastros
 {
